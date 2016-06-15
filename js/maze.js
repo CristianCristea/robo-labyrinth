2fa902815f1889f8f7fa15c5d3565b4148febe8d
Cristian Cristea <cristiancristea.1987@gmail.com>
2016-06-15 21:20:33 +0200

    Initial commit, create maze obj

---
+++ js/maze.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function Maze(width, height) {
+  this.width = width;
+  this.height = height;
+
+  this.startX           = null;
+  this.startY           = null;
+  this.startOrientation = null;
+  this.endX             = null;
+  this.endY             = null;
+
+}
+
+// setter methods
+Maze.prototype.setStart = function(x, y, orientation) {
+  this.startX = x;
+  this.startY = y;
+  this.startOrientation = orientation;
+};
+
+Maze.prototype.setEnd = function(x, y) {
+  this.endX = x;
+  this.endY = y;
+};
